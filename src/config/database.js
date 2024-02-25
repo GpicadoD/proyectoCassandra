a5f9b76fc8093c9e7e48bc19bced976e90744f95
GpicadoD <128370671+GpicadoD@users.noreply.github.com>
2024-02-25 19:09:22 +0100

    Quitando comentarios

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 import { Client } from 'cassandra-driver';
 
 const client = new Client({
-    contactPoints: ['127.0.0.1'], // Puedes especificar aquí los nodos de tu clúster de Cassandra
-    localDataCenter: 'datacenter1', // Especifica el datacenter local si es relevante para tu configuración
-    keyspace: 'jsonfiles' // Especifica el keyspace que quieres utilizar
+    contactPoints: ['127.0.0.1'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'jsonfiles'
 });
 
-// Función para conectar a Cassandra
+
 async function conectarCassandra() {
     try {
         await client.connect();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import Jsonrouter from "./routes/jsonRoutes.js";
-import client from "./config/database.js"; // Importa el cliente de Cassandra
+import client from "./config/database.js";
 
 const app = express();
 
-// Configuraciones
+
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
-//Configuramos para que la vista sea mediante modelo ejs.
+
 app.set('view engine', 'ejs');
 
-// configura la vista para que sea ejs
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Routes
+
 app.use(Jsonrouter);
 
 app.use(express.static('public'));
 
-//Iniciando el servidor, escuchando...
+
 app.listen(app.get('port'), async () => {
-    await client.connect(); // Conecta a Cassandra antes de iniciar el servidor Express
+    await client.connect();
     console.log(`Server listening on port ${app.get('port')}`);
 });
